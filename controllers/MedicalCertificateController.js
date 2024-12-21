4a2100a19b9538a11d4e19d47f6365596ef25035
Ethan-Chew <ethanchewmh@gmail.com>
2024-12-21 15:55:29 +0800

    feat: upload medical certificate

--- controllers/MedicalCertificateController.js
+++ controllers/MedicalCertificateController.js
@@ -1,9 +1,14 @@
+import firebase from '../firebase.js';
+import { collection, setDoc, doc } from 'firebase/firestore';
+import retrieveDataFromMC from '../utils/retrieveDataFromMC.js';
+import uploadImage from '../utils/uploadImage.js';
+import { v4 as uuidv4 } from 'uuid';
 import dotenv from 'dotenv';
 dotenv.config();
 
 async function uploadMedicalCertificate(req, res) {
     try {
-        const medicalCertImage = req.mc;
+        const medicalCertImage = req.file;
         const userId = req.body.userId;
 
         if (medicalCertImage === undefined || userId === undefined) {
@@ -13,7 +18,26 @@ async function uploadMedicalCertificate(req, res) {
             }
         }
 
+        const imageUrl = await uploadImage(medicalCertImage, "medicalcertificates", userId);
+        const recognitionData = await retrieveDataFromMC(imageUrl);
         
+        const mcData = {
+            id: uuidv4(),
+            userId: userId,
+            imageUrl: imageUrl,
+            patientName: recognitionData.patientName,
+            startDate: recognitionData.startDate,
+            duration: recognitionData.duration,
+            clinicName: recognitionData.clinicName,
+            status: "pending"
+        }
+
+        const docRef = doc(firebase.db, "medicalCertificates", mcData.id);
+        await setDoc(docRef, mcData);
+        return res.status(200).json({
+            status: "success",
+            mcData: mcData,
+        });
     } catch (err) {
         return res.status(err.status ? err.status : 500).json({ error: err.message });
     }
--- utils/retrieveDataFromMC.js
+++ utils/retrieveDataFromMC.js
@@ -7,38 +7,53 @@ const client = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 })
 
-export default async function retrieveDataFromMC(medicalCertImage) {
-    // Create an assistant to read the medical certificate
-    const assistant = await client.beta.assistants.create({
-        name: "Medical Certificate Data Assistant",
-        instructions: `
-            You are an analyst supposed to read various information from a medical certificate provided to you.
-            Read the Medical Certificate, and provide the following information in an array format, in the order of:
-            1. Patient Name
-            2. Start Date of the Medical Certificate but convert it to UNIX time in Miliseconds
-            3. Duration of the Medical Certificate
-            4. Clinic Name
-            It should look something like: John Doe, 1640995200000, 7, Medical Clinic
-        `,
+const systemPrompt =  `
+    Read the Medical Certificate, and provide the following information in the order of:
+    1. Patient Name
+    2. Start Date of the Medical Certificate but convert it to UNIX time in Miliseconds
+    3. Duration of the Medical Certificate
+    4. Clinic Name
+    The format should be: 'John Doe, 1640995200000, 7, Medical Clinic' FOLLOW IT EXACTLY, NO TEXT FORMATTING.
+`;
+
+export default async function retrieveDataFromMC(medicalCertUrl) {
+    const data = await client.chat.completions.create({
         model: "gpt-4o-mini",
-        tools: [{ type: "file_search" }]
-    });
-
-    
-    const medicalCertificateFileData = await client.files.create({
-        file: fs.createReadStream(medicalCertImage.path),
-        purpose: "assistants"
-    })
-
-    const thread = await client.beta.threads.create({
         messages: [
             {
+                role: "system",
+                content: [
+                    {
+                        type: "text",
+                        text: systemPrompt
+                    }
+                ]
+            },
+            {
                 role: "user",
-                content: "Read the medical certificate and provide the required information",
-                attachments: [{ file_id: medicalCertificateFileData.id, tools: [{ type: "file_search" }] }]
+                content: [
+                    {
+                        type: "text",
+                        text: "Give me the required information from this image."
+                    },
+                    {
+                        type: "image_url",
+                        image_url: {
+                            url: medicalCertUrl
+                        }
+                    }
+                ]
             }
         ]
-    })
+    }).withResponse();
 
-    
+    const response = data.data.choices[0].message.content;
+    const responseArray = response.split(", "); 
+    const formattedResponse = {
+        patientName: responseArray[0],
+        startDate: responseArray[1],
+        duration: responseArray[2],
+        clinicName: responseArray[3]
+    }
+    return formattedResponse;
 }
